186c2b46a845340b7570e5a531069bf86d162b81
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-03-05 19:04:47 +0700

    Fix deadlock with mutex

--- client.go
+++ client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -37,34 +38,50 @@ type connection struct {
 
 	// Buffered channel of outbound messages.
 	send chan []byte
+	mu   sync.Mutex
 }
 
 // readPump pumps messages from the websocket connection to the hub.
 func (s subscription) readPump() {
 	c := s.conn
 	defer func() {
+		//fmt.Println("defer di readpump")
 		h.unregister <- s
 		c.ws.Close()
 	}()
 	c.ws.SetReadLimit(maxMessageSize)
-	c.ws.SetReadDeadline(time.Now().Add(pongWait))
-	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
-	for {
-		_, msg, err := c.ws.ReadMessage()
-		if err != nil {
-
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-				//log.Printf("error waktu nge read : unexpectedcloseerror")
+	err := c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	if err != nil {
+		//log.Printf("error saat setReadDeadline")
+	} else {
+		c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+		for {
+			c.mu.Lock()
+			_, msg, err := c.ws.ReadMessage()
+			c.mu.Unlock()
+			if err != nil {
+				//fmt.Println("exit")
+				//c.ws.Close()
+				c.mu.Lock()
+				c.write(websocket.CloseMessage, []byte{})
+				c.mu.Unlock()
+				//log.Printf("ada error nih waktu nge read message dari ws")
 				//log.Printf("error: %v", err)
-			} else {
-				log.Printf("ada error nih waktu nge read message dari ws")
-				log.Printf("error: %v", err)
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+					//log.Printf("error waktu nge read : unexpectedcloseerror")
+					//log.Printf("error: %v", err)
+				} else {
+					// log.Printf("ada error nih waktu nge read message dari ws")
+					// log.Printf("error: %v", err)
+				}
+				break
 			}
-			break
+			m := message{msg, s.room}
+			h.broadcast <- m
 		}
-		m := message{msg, s.room}
-		h.broadcast <- m
+
 	}
+
 }
 
 // write writes a message with the given message type and payload.
@@ -79,25 +96,40 @@ func (s *subscription) writePump() {
 	c := s.conn
 	ticker := time.NewTicker(pingPeriod)
 	defer func() {
+		//fmt.Println("defer di writepump")
 		ticker.Stop()
 		c.ws.Close()
 	}()
 	for {
 		select {
-		case message, ok := <-c.send: //jika ada pesan masuk ke dalam room
+		case message, ok := <-c.send: //jika ada pesan masuk ke dalam channel send di koneksi tertentu, maka kirim pesan tsb ke websocket
+			//fmt.Println("ada pesan masuk ke dalam room")
+			//fmt.Println("ok atau tidak", ok)
+			//fmt.Println("messagenya adalah : ", message)
+
 			if !ok {
+				c.mu.Lock()
 				c.write(websocket.CloseMessage, []byte{}) //write ke ws, tipe write nya adalah close message, isinya adalah {}
+				c.mu.Unlock()
 				return
 			}
-			if err := c.write(websocket.TextMessage, message); err != nil { //coba write ke ws, tipenya adalah textmessage, isinya adalah variabel message, kalau tidak error maka
+			c.mu.Lock()
+			err := c.write(websocket.TextMessage, message)
+			c.mu.Unlock()
+			if err != nil { //coba write ke ws, tipenya adalah textmessage, isinya adalah variabel message, kalau tidak error maka
 				return
 			}
+
 		case <-ticker.C: //keknya, kalau roomnya timeout maka
-			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+			c.mu.Lock()
+			err := c.write(websocket.PingMessage, []byte{})
+			c.mu.Unlock()
+			if err != nil {
 				return
 			}
 		}
 	}
+
 }
 
 // serveWs handles websocket requests from the peer.
