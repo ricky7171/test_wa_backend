53ba1eca1bafa8b187666b07b3f8824632cc0c93
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-03-07 12:08:47 +0700

    Remove unused comment and unused route

--- main.go
+++ main.go
@@ -6,17 +6,11 @@ import (
 
 func main() {
 	go h.run()
-	// fmt.Println("h.run nih")
 	router := gin.New()
 	router.LoadHTMLFiles("index.html")
 
 	router.GET("/room/:roomId", func(c *gin.Context) {
-		// fmt.Println("ada yang masuk room", c.Param("roomId"))
 		c.HTML(200, "index.html", nil)
-	})
-
-	router.GET("/roomcoba1", func(c *gin.Context) {
-		c.JSON(200, "{error:false}")
 	})
 
 	//sebenernya bukan nge get sih. tapi "pada saat ada client yang konek ke 'topik' roomId ini, maka jadikan client ini sebagai subscriber"
@@ -24,7 +18,6 @@ func main() {
 	//client juga bisa kirim data (dengan catatan sudah terdaftar di list subscriber) ke topic roomId tsb
 
 	router.GET("/ws/:roomId", func(c *gin.Context) {
-		//fmt.Println("ada yang konek ke ws nya room")
 		roomId := c.Param("roomId")
 		serveWs(c.Writer, c.Request, roomId)
 	})
