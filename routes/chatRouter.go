3ab85268d9471d88a0137b7b8de78044849c0349
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-03-16 13:55:26 +0700

    Add last ID on get chat API for pagination

--- routes/chatRouter.go
+++ routes/chatRouter.go
@@ -9,7 +9,7 @@ import (
 
 func ChatRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authentication())
-	incomingRoutes.GET("/api/chat/:room_id", controller.GetChat())
+	incomingRoutes.GET("/api/chat/:room_id/:last_id", controller.GetChat())
 	incomingRoutes.POST("/api/new_chat", controller.NewMessage())
 	incomingRoutes.GET("/api/contact", controller.GetContact())
 }
