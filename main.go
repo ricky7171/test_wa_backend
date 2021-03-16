fd3c7c11703f555f6a25135f2894fce30fd61a3c
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-03-16 13:54:13 +0700

    Remove unused code & comments

--- main.go
+++ main.go
@@ -19,94 +19,29 @@ type RegisterRequest struct {
 }
 
 func main() {
-	// if err := agent.Listen(agent.Options{
-	// 	ShutdownCleanup: true, // automatically closes on os.Interrupt
-	// }); err != nil {
-	// 	log.Fatal(err)
-	// }
 
-	//setup
+	//run hub to listen data chat websocket on channel
 	go hub.MainHub.Run()
+
+	//make router instance
 	router := gin.New()
+
+	//load all html
 	router.LoadHTMLGlob("views/*")
 
-	// Global middleware
-	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
-	// By default gin.DefaultWriter = os.Stdout
+	// Middleware that used to log all request on terminal
 	router.Use(gin.Logger())
 
-	// Recovery middleware recovers from any panics and writes a 500 if there was one.
-	// router.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
-	// 	if err, ok := recovered.(string); ok {
-	// 		c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
-	// 	}
-	// 	c.String(http.StatusInternalServerError, fmt.Sprintf("Panic !"))
-	// }))
-
 	//front-end router
-	router.GET("/login", func(c *gin.Context) {
-		c.HTML(200, "login.html", nil)
-	})
-
-	router.GET("/register", func(c *gin.Context) {
-		c.HTML(200, "register.html", nil)
-	})
-
-	router.GET("/home", func(c *gin.Context) {
-		c.HTML(200, "home.html", nil)
-	})
-
-	router.GET("/room/:roomId", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
-	})
+	routes.ViewRoutes(router)
 
 	//websocket router
-	//sebenernya bukan nge get sih. tapi "pada saat ada client yang konek ke 'topik' roomId ini, maka jadikan client ini sebagai subscriber"
-	//jadi kayak mendaftarkan client ke topik nya. Setiap ada update dari topic, maka client yang berlangganan akan dikasi tau.
-	//client juga bisa kirim data (dengan catatan sudah terdaftar di list subscriber) ke topic roomId tsb
-
-	router.GET("/ws/:user_id", func(c *gin.Context) {
-		userID := c.Param("user_id")
-		hub.ServeWs(c.Writer, c.Request, userID)
-	})
+	routes.WebSocketRouter(router)
 
 	//REST API router
 	routes.AuthRoutes(router)
 	routes.ChatRoutes(router)
 
-	// router.POST("/api/auth/login", func(c *gin.Context) {
-	// 	var loginRequest LoginRequest
-	// 	if err := c.ShouldBindJSON(&loginRequest); err != nil {
-	// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	c.JSON(200, gin.H{
-	// 		"error":    false,
-	// 		"message":  "Success Login",
-	// 		"phone":    loginRequest.Phone,
-	// 		"password": loginRequest.Password,
-	// 	})
-	// })
-
-	// router.POST("/api/auth/register", func(c *gin.Context) {
-	// 	var registerRequest RegisterRequest
-	// 	if err := c.ShouldBindJSON(&registerRequest); err != nil {
-	// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	c.JSON(200, gin.H{
-	// 		"error":    false,
-	// 		"message":  "Success Register",
-	// 		"phone":    registerRequest.Phone,
-	// 		"password": registerRequest.Password,
-	// 	})
-	// })
-
-	router.POST("/api/getContacts", func(c *gin.Context) {
-
-	})
-
+	//run server on port 8080
 	router.Run("0.0.0.0:8080")
 }
