9b46c0848443b1fcb12552d1c70fdaa3660967d9
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-03-16 13:54:00 +0700

    Update save chat API

--- hub/hub.go
+++ hub/hub.go
@@ -3,7 +3,7 @@ package hub
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 	"time"
 	"wa/database"
 	"wa/models"
@@ -17,10 +17,8 @@ import (
 var roomCollection *mongo.Collection = database.OpenCollection(database.Client, "rooms")
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
 
-//contoh subscription
-// {con : con1, userId : 1}
-// jadi semacam data antara room dan koneksi
-// baik register maupun unregister, tipe datanya ya subscription
+//subscription example
+// {con : con1, userId : xxx123}
 type subscription struct {
 	conn   *connection
 	userId string
@@ -51,23 +49,24 @@ var MainHub = Hub{
 }
 
 func SaveChat(msg models.Message) error {
-	//1. buat ctx dengan timeout 100 detik
-	//dimana ctx ini bakal digunakan saat nge query
+	//1. make ctx object
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 
-	//2. isi created_at nya dulu
+	//2. fill created_at
 	msg.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 
-	//3. jika didalam object msg belum ada room_id nya,
-	//maka cek apakah antara id pengirim dan penerima itu sudah memiliki room sebelumnya
-	//kalau belum ya berarti buat room baru
-	makeNewRoom := false //=> artinya tidak usah bikin room baru
+	//3. if room_id not found in msg object, then find room that used by sender and receiver
+	//if room not found, then make new room
+	newFromUserIdObject, err := primitive.ObjectIDFromHex(msg.FromUserId)
+	newToUserIdObject, err := primitive.ObjectIDFromHex(msg.ToUserId)
+
+	makeNewRoom := false //=> means, no need to make new room
 	if msg.Room_id == "" {
 		var roomExists models.Room
 		opts := options.FindOne().SetProjection(bson.M{
 			"room_id": 1,
 		})
-		fmt.Println("cari room apakah sudah ada atau belum")
 		err := roomCollection.FindOne(
 			ctx,
 			bson.M{
@@ -75,20 +74,20 @@ func SaveChat(msg models.Message) error {
 					bson.M{
 						"$and": []interface{}{
 							bson.M{
-								"users.0.user_id": msg.FromUserId,
+								"users.0": newFromUserIdObject,
 							},
 							bson.M{
-								"users.1.user_id": msg.ToUserId,
+								"users.1": newToUserIdObject,
 							},
 						},
 					},
 					bson.M{
 						"$and": []interface{}{
 							bson.M{
-								"users.1.user_id": msg.FromUserId,
+								"users.1": newFromUserIdObject,
 							},
 							bson.M{
-								"users.0.user_id": msg.ToUserId,
+								"users.0": newToUserIdObject,
 							},
 						},
 					},
@@ -97,49 +96,53 @@ func SaveChat(msg models.Message) error {
 			opts,
 		).Decode(&roomExists)
 		defer cancel()
-
-		if err != nil { //artinya roomnya kosong
+		if err != nil { //means, room not found, then need to make new room
 			makeNewRoom = true
-		} else { //kalau ternyata ketemu roomnya
-			msg.Room_id = roomExists.Room_id
+		} else { //means, room found, then NO need to make new room
+			msg.Room_id = roomExists.ID.Hex()
 		}
 
 	}
 
-	//jika roomnya sudah ada maka, cukup update field chat_history, lalu insert data chat disitu
+	//4. if room found, then update field chat_history and insert chat in that field
 	if !makeNewRoom {
-		roomCollection.UpdateOne(ctx, bson.M{
-			"room_id": msg.Room_id,
+		roomObjectID, _ := primitive.ObjectIDFromHex(msg.Room_id)
+		_, err := roomCollection.UpdateOne(ctx, bson.M{
+			"_id": roomObjectID,
 		},
 			bson.M{
 				"$push": bson.M{
 					"chat_history": bson.M{
+						"_id":        primitive.NewObjectID(),
 						"user_id":    msg.FromUserId,
 						"message":    msg.Data,
 						"created_at": msg.Created_at,
 					},
 				},
 			})
+		if err != nil {
+			log.Fatal(err)
+		}
 		defer cancel()
-	} else { //jika roomnya belum ada, maka insert room baru dengan chat_history didalamnya
+	} else { //5. if room not found, then insert new room also with that chat inside
 		newRoomId := primitive.NewObjectID()
-		newFromUserIdObject, err := primitive.ObjectIDFromHex(msg.FromUserId)
+
 		if err != nil {
 			return nil
 		}
-		newToUserIdObject, err := primitive.ObjectIDFromHex(msg.ToUserId)
+
 		if err != nil {
 			return nil
 		}
 		roomCollection.InsertOne(ctx, bson.M{
-			"_id":     newRoomId,
-			"room_id": newRoomId.Hex(),
+			"_id": newRoomId,
 			"users": []interface{}{
 				newFromUserIdObject,
 				newToUserIdObject,
 			},
 			"chat_history": []interface{}{
 				bson.M{
+					"_id":        primitive.NewObjectID(),
 					"user_id":    msg.FromUserId,
 					"message":    msg.Data,
 					"created_at": msg.Created_at,
@@ -156,16 +159,16 @@ func SaveChat(msg models.Message) error {
 func (h *Hub) Run() {
 	for {
 		select {
-		case s := <-h.Register: //kalau ada subs yg register (dg kata lain, kalau ada user baru yg konek ke ws)
-			h.Users[s.userId] = s.conn //kalaupun sebelumnya sudah terisi, ya langsung ke replace
-		case s := <-h.Unregister: //kalau ada subs yang unregister (dg kata lain, kalau ada user yang exit dari koneksi ws)
+		case s := <-h.Register: //when there is user connect to ws
+			h.Users[s.userId] = s.conn //fill h.users with key that user id, then fill value with ws connection
+		case s := <-h.Unregister: //when there is user disconnect from ws
 			if _, ok := h.Users[s.userId]; ok {
 				close(s.conn.send)
 				delete(h.Users, s.userId)
 			}
-		case m := <-h.Broadcast: //kalau ada broadcast/message masuk
+		case m := <-h.Broadcast: //when there is message
 
-			//1. store to database dulu
+			//1. store to database
 			go SaveChat(m)
 
 			//2. convert object m ke byte[]
@@ -174,25 +177,25 @@ func (h *Hub) Run() {
 				panic(err)
 			}
 
-			//kirim ke websocketnya
-			//3. kirim ke ws nya pengirim dulu
+			//3. send to ws
+			//3.a. send to sender ws
 			if _, ok := h.Users[m.FromUserId]; ok {
 				select {
-				case h.Users[m.FromUserId].send <- dataSend: //kirim pesan ke pengirim (melalui channel send)
-				default: //kalau gagal (mungkin pengirimnya abis kirim pesan, langsung diexit browsernya)
+				case h.Users[m.FromUserId].send <- dataSend: //send message to sender through "send" channel
+				default: //this is when fail to send the message (maybe user exit from browser)
 					break
 				}
-			} else { //mungkin pengirimnya sudah tidak aktif lagi
+			} else {
 				break
 			}
 
-			//4. baru kirim ke wsnya penerima
+			//3.b. send to receiver ws
 			if _, ok := h.Users[m.ToUserId]; ok {
 				select {
-				case h.Users[m.ToUserId].send <- dataSend: //kirim pesan ke penerimanya (melalui channel send)
-				default: //kalau gagal (mungkin penerimanya error koneksinya, mending hapus aja)
+				case h.Users[m.ToUserId].send <- dataSend: //send message to receiver through "send" channel
+				default: //this is when fail to send the message (maybe user exit from browser)
 				}
-			} else { //mungkin penerimanya sudah tidak aktif lagi
+			} else {
 			}
 
 		}
