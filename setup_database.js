f02f7808410f28494b0e93ea7b6e9c68bf4fb69c
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-03-19 15:57:27 +0700

    Update setup database.js

--- setup_database.js
+++ setup_database.js
@@ -4,12 +4,25 @@ use waku1;
 //2. create users collection
 db.createCollection("users");
 
-//3. create rooms collection
-db.createCollection("rooms");
+//3. create contacts collection
+db.createCollection("contacts");
 
-//4. add index to users collection
+//4. create chats collection
+db.createCollection("chats");
+
+//5. add index to users collection
 db.users.createIndex({
     "phone": 1
 }, {
     unique: true
+});
+
+//6. add index to contacts collection
+db.contacts.createIndex({
+    "users": 1
+});
+
+//7. add index to contact_id on chats collection
+db.chats.createIndex({
+    "contact_id": 1
 });
