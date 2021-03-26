9eb7310735d56b2263dacbfc674ed9ea70a88b69
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-03-26 07:56:36 +0700

    Change url naming convention

--- routes/chatRouter.go
+++ routes/chatRouter.go
@@ -9,6 +9,6 @@ import (
 func ChatRoutes(incomingRoutes *gin.RouterGroup) {
 	incomingRoutes.GET("/ws/:userId", controller.ConnectWs())
 	incomingRoutes.GET("/api/chat/:contactId/:lastId", controller.GetChat())
-	incomingRoutes.POST("/api/new_chat", controller.NewMessage())
+	incomingRoutes.POST("/api/new-chat", controller.NewMessage())
 	incomingRoutes.GET("/api/contact", controller.GetContact())
 }
