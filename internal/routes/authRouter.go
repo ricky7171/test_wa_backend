5d9e2866caea54056dbe91d6da82b72778411a52
Ricky <samuel.ricky@ti.ukdw.ac.id>
2021-03-31 13:49:35 +0700

    Add checktoken route

--- internal/routes/authRouter.go
+++ internal/routes/authRouter.go
@@ -11,4 +11,5 @@ func AuthRoutes(incomingRoutes *gin.Engine, dbInstance *mongo.Database) {
 	incomingRoutes.POST("/api/auth/register", controller.Register(dbInstance))
 	incomingRoutes.POST("/api/auth/login", controller.Login(dbInstance))
 	incomingRoutes.POST("/api/auth/refresh-token", controller.RefreshToken(dbInstance))
+	incomingRoutes.POST("/api/auth/check-token", controller.CheckToken(dbInstance))
 }
